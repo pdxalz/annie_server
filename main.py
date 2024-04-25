cdfa54bb061b39ca955b48a93125d8b10cd41ded
Alan Zimmerman <pdxalz@gmail.com>
2024-04-25 09:24:47 -0700

    Fixed tide missing 4th table entry.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #  docker-compose up
 #   (cd to Desktop/annie_api/)
-#  docker build . -t anniem
-#  docker run --rm -v $PWD/winddata:/winddata -p 80:8000/tcp -e SERVER_URL=http://192.168.68.113 -v roosterpict:/rooster  anniem
+#  docker build . -t annie_img
+#  docker run --rm -v $PWD/winddata:/winddata -p 80:8000/tcp -e SERVER_URL=http://192.168.68.113 -v roosterpict:/rooster  annie_img
 #  docker ps -a
 #   docker system prune -a      (wipe out all data)
 #   sudo find / -name test.db   (find the location of the database)
